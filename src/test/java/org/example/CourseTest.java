264868b6479274132df28a520be12795e6e472f2
p.p <38204935+p0mp4n@users.noreply.github.com>
2023-03-06 23:59:27 +0900

    학점계산기 프로토타입

---
+++ src/test/java/org/example/CourseTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+public class CourseTest {
+    @DisplayName("과목(코스)를 테스트한다.")
+    @Test
+    void createTest() {
+        assertThatCode(() -> new Course("OOP", 3, "A+"))
+                .doesNotThrowAnyException();
+    }
+}
